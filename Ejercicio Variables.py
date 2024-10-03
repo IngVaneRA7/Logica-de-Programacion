a727a6134ef08fc78f651ac245f6b387b31ed9f8
Ingrid Vanessa Rueda <157174223+IngVaneRA7@users.noreply.github.com>
2024-10-02 21:51:18 -0500

    Add files via upload

---
+++ Ejercicio Variables.py
@@ -0,0 +1,19 @@
+# varible: una variable es un espacio en la memoria del computador que me permite almacenar un dato
+# para definir una variable, tengo que ponerle un nombre y asignarle un valor, por ejemplo:
+# mi variable se llama saludo y su valor es "Hola, mundo"
+
+"""
+En Python existen 4 tipos de datos primitivos los cuales son:
+1. string: el cual hace referencia a una cadena de texto y su valor debe ir entre comillas simples o dobles 
+2. int: el cual hace referencia a los números enteros y no deben ir entre ningún caracter
+3. boolean: el cual hace referencia a los valores "True" o "False"
+4. float: el caul hace referencia a los números decimales
+"""
+
+saludo = "Hola, mundo"
+print (saludo)
+num1 = "7"
+print (num1)
+
+
+
